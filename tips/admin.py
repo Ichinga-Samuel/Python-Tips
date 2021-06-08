286c17e9b8d2c28842e06c5f70f7675d2954355f
Ichinga Samuel <ichingasamuel@gmail.com>
2021-06-08 20:16:25 +0100

    initial commit

---
+++ tips/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from tips.models import Tips, Tags, Links
+# Register your models here.
+
+admin.site.register(Tips)
+admin.site.register(Tags)
+admin.site.register(Links)
---
+++ tips/urls.py
@@ -0,0 +1,12 @@
+from tips.views import TipsView, TipDetail, TagsView, SearchView
+from django.urls import path
+
+
+app_name = 'tips'
+
+urlpatterns = [
+    path('tags/<tag>/', TagsView.as_view(), name='tag'),
+    path('search/', SearchView.as_view(), name='search'),
+    path('', TipsView.as_view(), name='main'),
+    path('tips/<int:pk>/', TipDetail.as_view(), name='tip'),
+]
